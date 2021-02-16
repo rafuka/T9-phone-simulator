1c65b4f50a4b967a52f4e53b4ae92ed7a45d2b01
rafuka <rafuk89@gmail.com>
2021-02-16 21:23:50 +0100

    Basic setup: Webpack, Babel, SASS modules and postCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+    console.log('on App!');
+    return (
+        <h1>Hello Kiwi!</h1>
+    );
+};
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+    },
+};
