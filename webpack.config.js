b0c37a8efc556603487bc6b4fa0acbd1191b2440
rafuka <rafuk89@gmail.com>
2021-02-18 22:23:23 +0100

    added webpack config for CSS modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,66 @@
 const path = require('path');
 
-module.exports = {
-    entry: path.resolve(__dirname, 'frontend/src/index.js'),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-    },
-    output: {
-        path: path.resolve(__dirname, './frontend/dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, './frontend/dist'),
-    },
+module.exports = (env) => {
+    const isDevelopment = env.development;
+
+    return {
+        entry: path.resolve(__dirname, 'frontend/src/index.js'),
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.scss$/,
+                    exclude: /\.global\.scss$/,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    localIdentName: '[name]_[local]_[hash:base64:5]'
+                                },
+                                sourceMap: isDevelopment,
+                            },
+                        },
+                        'postcss-loader',
+                        {
+                            loader:'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment,
+                            },
+                        }
+                    ],
+                },
+                {
+                    test: /\.global\.scss$/,
+                    exclude: /^\.scss$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'postcss-loader',
+                        {
+                            loader:'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment,
+                            },
+                        }
+                    ]
+                }
+            ],
+        },
+        resolve: {
+            extensions: ['*', '.js', '.jsx'],
+        },
+        output: {
+            path: path.resolve(__dirname, './frontend/dist'),
+            filename: 'bundle.js',
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, './frontend/dist'),
+        },
+    }
 };
