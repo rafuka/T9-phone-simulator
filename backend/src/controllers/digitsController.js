9154d11f1c138f5e359bd9f9e7d7e6f87cccecf0
rafuka <rafuk89@gmail.com>
2021-02-18 21:22:53 +0100

    changed response to json instead of using JSON.stringify

--- backend/src/controllers/digitsController.js
+++ backend/src/controllers/digitsController.js
@@ -10,14 +10,14 @@ digitsController.getDictionaryWords = function(req, res) {
     const { digits } = req.params;
     const words = trieDictionary.getWords(digits);
     res.status(200);
-    res.end(JSON.stringify(words));
+    res.json(words);
 };
 
 digitsController.getNaiveCombinations = function(req, res) {
     const { digits } = req.params;
     const words = naiveCombinations(digits);
     res.status(200);
-    res.end(JSON.stringify(words));
+    res.json(words);
 };
 
 export default digitsController;
